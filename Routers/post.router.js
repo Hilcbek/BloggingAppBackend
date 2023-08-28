ff59772a9b6b9a7a6a35cf233bf279d0e06fc832
Hilcbek <balemayehu07@gmail.com>
2023-08-28 15:53:29 +0300

    added backend 3

--- Routers/post.router.js
+++ Routers/post.router.js
@@ -3,7 +3,7 @@ import { AddBlog, AllBlogs, DeletePost, EditPost, QuerySearch, RelatedBlogs, Sin
 import { IsUserLoggedIn } from '../Token/token.js';
 export let blogRouter = express.Router()
 blogRouter.post('/',IsUserLoggedIn,AddBlog);
-blogRouter.get('/',IsUserLoggedIn,AllBlogs);
+blogRouter.get('/',AllBlogs);
 blogRouter.get('/search',IsUserLoggedIn,QuerySearch);
 blogRouter.get('/searchById/:id',IsUserLoggedIn,SingleBlogById);
 blogRouter.get('/searchBytitle/:title',IsUserLoggedIn,SingleBlog);
