a22b46e86105894eeda7eca0c27c64a1388d6ddf
Hilcbek <balemayehu07@gmail.com>
2023-08-28 16:01:45 +0300

    added backend 3

--- Routers/tag.router.js
+++ Routers/tag.router.js
@@ -2,5 +2,5 @@ import express from 'express'
 import { AddTags, AllTags } from '../controllers/tag.controller.js';
 import { IsUserLoggedIn } from '../Token/token.js';
 export let tagRouter = express.Router()
-tagRouter.post('/',IsUserLoggedIn,AddTags);
-tagRouter.get('/',IsUserLoggedIn,AllTags)
+tagRouter.post('/',AddTags);
+tagRouter.get('/',AllTags)
